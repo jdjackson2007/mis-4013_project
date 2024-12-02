df3308a74d2266e03abdf32b725808fdaa1e32b9
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-01 22:58:51 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -19,9 +19,18 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="explorecorps.php">Explore Corps</a></li>
-                        <li class="nav-item"><a class="nav-link" href="notable_lanterns.php">Notable Lanterns</a></li>
-                        <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                        
+                        <!-- Dropdown Menu -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Pages
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="explorecorps.php">Explore Corps</a></li>
+                                <li><a class="dropdown-item" href="notable_lanterns.php">Notable Lanterns</a></li>
+                                <li><a class="dropdown-item" href="about.php">About</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -32,3 +41,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
