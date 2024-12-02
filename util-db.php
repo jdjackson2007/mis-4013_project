cab64c316cb31930b0e9ba5ca78c2c89a0e39dc7
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-01 20:31:32 -0600

    Create util-db.php

---
+++ util-db.php
@@ -0,0 +1,12 @@
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli( "mis4013project.mysql.database.azure.com", "jdjackson2007", "DougDoug07&&", "mis-4013_Project");
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
