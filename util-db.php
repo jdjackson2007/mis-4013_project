3728b2ed85f34e580847b801908624e74bc52bb0
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-01 20:40:04 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,11 +1,16 @@
 <?php
-function get_db_connection(){
+function get_db_connection() {
     // Create connection
-    $conn = new mysqli( "mis4013project.mysql.database.azure.com", "jdjackson2007", "DougDoug07&&", "mis-4013_Project");
-    
+    $conn = new mysqli(
+        "mis4013project.mysql.database.azure.com", 
+        "jdjackson2007",                          
+        "DougDoug07&&",                           
+        "mis-4013_Project"                        
+    );
+
     // Check connection
     if ($conn->connect_error) {
-      return false;
+        die("Connection failed: " . $conn->connect_error);
     }
     return $conn;
 }
