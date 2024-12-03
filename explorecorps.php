94a8d7aa5916348ec5005e9aa9c44e9bc1aacafc
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-02 18:47:36 -0600

    Update explorecorps.php

--- explorecorps.php
+++ explorecorps.php
@@ -1,16 +1,18 @@
 <?php
-// explorecorps.php
-// This file serves as the controller to fetch data about Lantern Corps 
-// and pass it to the corresponding view for rendering.
+
 
 require_once 'model-explorecorps.php';
 
-// Fetch all Lantern Corps data
+
 $corpsData = getAllCorps();
 
-// Total number of Corps (useful for the view)
+
+$emotions = getAllEmotions();
+$headquarters = getAllHeadquarters();
+
+
 $totalCorps = count($corpsData);
 
-// Include the view file
+
 include 'view-explorecorps.php';
 ?>
