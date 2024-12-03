681d371a76def89516e8418421d09885ba840074
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-02 18:39:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,26 +4,26 @@ include "view-header.php";
 ?>
 
 <!-- Full-page container with background -->
-<div class="container-fluid text-center text-white" style="background-image: url('https://3.bp.blogspot.com/-UbPydZyEqig/USxPpJVNBJI/AAAAAAAAE_Q/oKj7rgMDbzg/s1600/Lantern_Corps_Spectrum_by_tytemp1980.jpg'); background-size: cover; background-position: center; height: 100vh;">
+<div class="container-fluid text-center text-white full-page-background" role="img" aria-label="Lantern Corps spectrum background image">
 
     <!-- Buttons Section -->
     <div class="d-flex flex-column justify-content-center align-items-center" style="height: 100%;">
 
         <!-- Home Button -->
         <div class="text-center mb-4 w-75">
-            <a href="index.php" class="btn btn-light btn-lg w-100 text-dark">Home</a> <!-- Full-width button -->
+            <a href="index.php" class="btn btn-light btn-lg w-100 text-dark" aria-label="Go to Home Page">Home</a>
         </div>
 
         <!-- Navigation Buttons Section -->
-        <div class="row w-75">
-            <div class="col-md-4">
-                <a href="explorecorps.php" class="btn btn-warning btn-lg w-100 mb-3">Explore Corps</a> <!-- Yellow button -->
+        <div class="row row-cols-1 row-cols-md-3 w-75">
+            <div class="col">
+                <a href="explorecorps.php" class="btn btn-warning btn-lg w-100 mb-3" aria-label="Explore Lantern Corps">Explore Corps</a>
             </div>
-            <div class="col-md-4">
-                <a href="notable_lanterns.php" class="btn btn-light btn-lg w-100 mb-3 text-dark">Notable Lanterns</a> <!-- White button -->
+            <div class="col">
+                <a href="notable_lanterns.php" class="btn btn-light btn-lg w-100 mb-3 text-dark" aria-label="View Notable Lanterns">Notable Lanterns</a>
             </div>
-            <div class="col-md-4">
-                <a href="about.php" class="btn btn-indigo btn-lg w-100 mb-3" style="background-color: indigo; color: white;">About the Project</a> <!-- Indigo button -->
+            <div class="col">
+                <a href="about.php" class="btn btn-indigo btn-lg w-100 mb-3" style="background-color: indigo; color: white;" aria-label="Learn About the Project">About the Project</a>
             </div>
         </div>
     </div>
