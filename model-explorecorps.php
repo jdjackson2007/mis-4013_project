864e53047b80b52aa5d4963734136f1af2e9e389
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-02 18:19:27 -0600

    Update model-explorecorps.php

--- model-explorecorps.php
+++ model-explorecorps.php
@@ -1,14 +1,5 @@
 <?php
-// model-explorecorps.php
 
-// Enable MySQLi error reporting for debugging
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-/**
- * Establish a database connection.
- *
- * @return mysqli Database connection object
- */
 function get_db_connection() {
     $conn = new mysqli("mis4013project.mysql.database.azure.com", "jdjackson2007", "DougDoug07&&", "lantern_corps");
 
@@ -22,11 +13,7 @@ function get_db_connection() {
     return $conn;
 }
 
-/**
- * Fetch all Lantern Corps with Emotion and Headquarters names.
- *
- * @return array Array of Lantern Corps data
- */
+
 function getAllCorps() {
     $conn = get_db_connection();
 
@@ -37,8 +24,7 @@ function getAllCorps() {
             corps_table.Corps_Name,
             emotions_table.Emotion_Name AS CorpsEmotion,
             headquarters_table.HQ_Name AS CorpsHQ,
-            corps_table.Corps_Description,
-            corps_table.Corps_Image_URL
+            corps_table.Corps_Description
         FROM corps_table
         JOIN emotions_table ON corps_table.CorpsEmotion_ID = emotions_table.Emotion_ID
         JOIN headquarters_table ON corps_table.CorpsHQ_ID = headquarters_table.HQ_ID
@@ -71,12 +57,12 @@ function addNewCorps($name, $emotion, $hq, $description, $imageUrl) {
 
     // Prepare the SQL statement
     $stmt = $conn->prepare("
-        INSERT INTO corps_table (Corps_Name, CorpsEmotion_ID, CorpsHQ_ID, Corps_Description, Corps_Image_URL)
-        VALUES (?, ?, ?, ?, ?)
+        INSERT INTO corps_table (Corps_Name, CorpsEmotion_ID, CorpsHQ_ID, Corps_Description)
+        VALUES (?, ?, ?, ?)
     ");
 
     // Bind parameters
-    $stmt->bind_param("siiss", $name, $emotion, $hq, $description, $imageUrl);
+    $stmt->bind_param("siiss", $name, $emotion, $hq, $description);
 
     // Execute the statement
     if (!$stmt->execute()) {
