ced7c2df340fb93d12b70e578e8e2dacdf2d9c53
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-03 20:18:11 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,5 +1,4 @@
-</div>
-        <p>&copy; 2024 Lantern Corps Universe | <a href="https://www.dccomics.com" target="_blank">Visit DC Comics</a></p>
-    </footer>
-  </body>
-</html>
+<!-- view-footer.php -->
+<footer>
+    <p>&copy; 2024 Lantern Corps Universe | <a href="https://www.dccomics.com" target="_blank">Visit DC Comics</a></p>
+</footer>
