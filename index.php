7f5f99f0cbac5dd3df9ed28af4436ece54092d0d
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-03 20:13:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -61,17 +61,5 @@
     </style>
 </head>
 <body>
-    <header>
-        <h1>Lantern Corps Universe</h1>
-        <p>Explore the endless stories of the Lantern Corps and the legendary Lanterns!</p>
-    </header>
-    <main>
-        <h2>Discover the Universe</h2>
-        <!-- Explore the Corps Section -->
-        <div class="button-group">
-            <a href="explore_corps.php">Explore the Corps</a>
-            <a href="lanterns.php">The Lanterns</a>
-            <a href="finding_the_comics.php">Finding the Comics</a>
-        </div>
-    </main>
- 
+   <header>
+        <footer>
