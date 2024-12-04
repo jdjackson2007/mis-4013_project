25ca2f48d92f6b94929465b98c0f60fc955a5dcb
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-03 19:26:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,8 @@
 $pageTitle = "Lantern Corps Home";
 include "view-header.php";
 ?>
-
 <div class="container-fluid text-center text-white home-background">
-
-    <!-- Buttons Section -->
     <div class="d-flex flex-column justify-content-center align-items-center" style="height: 100%;">
-
-        <!-- Navigation Buttons Section -->
         <div class="row w-75">
             <div class="col-md-4">
                 <a href="explorecorps.php" class="btn btn-warning btn-lg w-100 mb-3">Explore Corps</a>
@@ -22,5 +17,4 @@ include "view-header.php";
         </div>
     </div>
 </div>
-
 <?php include "view-footer.php"; ?>
