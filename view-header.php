812499906f45b1fac96782b00cc4ceed5d21e4e6
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-03 20:17:40 -0600

    Create view-header.php

---
+++ view-header.php
@@ -0,0 +1,13 @@
+<!-- view-header.php -->
+<header>
+    <h1>Lantern Corps Universe</h1>
+    <p>Explore the endless stories of the Lantern Corps and the legendary Lanterns!</p>
+</header>
+<main>
+    <h2>Discover the Universe</h2>
+    <div class="button-group">
+        <a href="explore_corps.php">Explore the Corps</a>
+        <a href="lanterns.php">The Lanterns</a>
+        <a href="finding_the_comics.php">Finding the Comics</a>
+    </div>
+</main>
