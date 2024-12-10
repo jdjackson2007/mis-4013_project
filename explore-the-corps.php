52651e4e1cb0bed91fcdad65359268ec54285807
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-10 13:30:54 -0600

    Update explore-the-corps.php

--- explore-the-corps.php
+++ explore-the-corps.php
@@ -1,8 +1,9 @@
 <?php
 Require_Once("util-db.php");
 Require_Once("model-explore-the-corps.php");
-$pageTitle = "The Corps";
 include "view-header.php";
+$pageTitle = "The Corps";
+
     $corpsList = getCorpsData();
 include "view-explore-the-corps.php";
 include "view-footer.php";
