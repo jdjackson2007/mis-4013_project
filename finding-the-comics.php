dfbbef6545f5bdfd2452e3eae48f62a3e0758154
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-11 15:54:01 -0600

    Update finding-the-comics.php

--- finding-the-comics.php
+++ finding-the-comics.php
@@ -1,3 +1,10 @@
+
 <?php
+require_once("model-explore-the-corps.php");
+
+$pageTitle = "The Corps";
+include "view-header.php";
 require_once 'view-finding-the-comics.php';
+
+include "view-footer.php";
 ?>
