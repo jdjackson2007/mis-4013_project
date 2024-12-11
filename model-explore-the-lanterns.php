0bed6deeb2e03b3f1f738c258ca2d4460a7f8760
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-11 13:43:53 -0600

    Update model-explore-the-lanterns.php

--- model-explore-the-lanterns.php
+++ model-explore-the-lanterns.php
@@ -2,7 +2,7 @@
 require_once 'util-db.php'; // Include the database connection utility
 
 /**
- * Fetch all Lantern data with relevant joins and multiple Corps affiliations
+ * Fetch all Lantern data from notablelanterns_table
  * 
  * @return mysqli_result The result set containing Lantern data
  * @throws Exception if the query fails
@@ -11,40 +11,21 @@ function getLanternsData() {
     try {
         $conn = get_db_connection(); // Get the database connection
 
-        // Simple SQL query
+        // Simple query to fetch data
         $query = "
             SELECT 
-                nl.NotableLanterns_Name AS name,
-                GROUP_CONCAT(nl.NotableLanterns_EarthVersion) AS earth_version,
-                GROUP_CONCAT(nl.NotableLanterns_Alias) AS alias,
-                GROUP_CONCAT(nl.NotableLanterns_Bio) AS bio,
-                GROUP_CONCAT(nl.NotableLanterns_FirstAppearance) AS first_appearance,
-                GROUP_CONCAT(nl.NotableLanterns_Status) AS status,
-                nl.NotableLanterns_MultipleCorps AS multiple_corps,
-                GROUP_CONCAT(c.Corps_Name) AS corps,
-                GROUP_CONCAT(cc.CorpsColor_Name) AS colors,
-                GROUP_CONCAT(ce.CorpsEmotion_Name) AS emotions,
-                GROUP_CONCAT(ch.CorpsHQ_Planet) AS planets,
-                GROUP_CONCAT(ch.CorpsHQ_Sector) AS sectors,
-                GROUP_CONCAT(lsc.LanternsSpecialClasses_ClassName) AS classes
+                NotableLanterns_ID AS id,
+                NotableLanterns_Name AS name,
+                Corps_ID AS corps,
+                NotableLanterns_EarthVersion AS earth_version,
+                NotableLanterns_Alias AS alias,
+                NotableLanterns_Bio AS bio,
+                NotableLanterns_FirstAppearance AS first_appearance,
+                NotableLanterns_Status AS status
             FROM 
-                notablelanterns_table nl
-            LEFT JOIN 
-                corps_table c ON FIND_IN_SET(c.Corps_ID, nl.NotableLanterns_MultipleCorps)
-            LEFT JOIN 
-                corpscolor_table cc ON c.CorpsColor_ID = cc.CorpsColor_ID
-            LEFT JOIN 
-                corpsemotion_table ce ON c.CorpsEmotion_ID = ce.CorpsEmotion_ID
-            LEFT JOIN 
-                corpshq_table ch ON c.CorpsHQ_ID = ch.CorpsHQ_ID
-            LEFT JOIN 
-                lanternclasses_table lc ON nl.NotableLanterns_ID = lc.NotableLanterns_ID
-            LEFT JOIN 
-                lanternspecialclasses_table lsc ON lc.SpecialClasses_ID = lsc.LanternSpecialClasses_ID
-            GROUP BY 
-                nl.NotableLanterns_Name, nl.NotableLanterns_MultipleCorps
+                notablelanterns_table
             ORDER BY 
-                nl.NotableLanterns_MultipleCorps, nl.NotableLanterns_Name;
+                NotableLanterns_Name;
         ";
 
         // Execute the query
