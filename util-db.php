9bd35553f51e6884a1506136048c2d952a46d59b
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-12-13 13:11:31 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,6 +1,6 @@
 <?php
 function get_db_connection() {
-    // Create connection
+    
     $conn = new mysqli(
         "mis4013project.mysql.database.azure.com", 
         "jdjackson2007",                          
@@ -8,7 +8,7 @@ function get_db_connection() {
         "lantern_corps"                        
     );
 
-    // Check connection
+    
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
